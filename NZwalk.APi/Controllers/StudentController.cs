574631e2202225d451d9120d6bb5dff16d498dc7
martinsharma17 <martinsharma18@gmail.com>
2025-04-25 01:07:21 +0545

    example

---
+++ NZwalk.APi/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZwalk.APi.Controllers
+{
+    //https:localhost:portno/api/student
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudent()
+        {
+            string[] studentnames = new string[] { "a" ,"b","c","d","e","f"};
+            return Ok(studentnames);
+        }
+    }
+}
